25051c7a199b6ac1c8f5fb4cf43f0d43c5b626eb
BilalQanoa <bilalqanoa@gmail.com>
2026-05-22 13:50:51 +0300

    practice CBV

--- practice_project/testapp/urls.py
+++ practice_project/testapp/urls.py
@@ -5,5 +5,9 @@ app_name = 'test'
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('form/', views.Form.as_view(), name='form'),
-    path('create/', views.CreatePerson.as_view(), name='create')
+    path('create/', views.CreatePerson.as_view(), name='create'),
+    path('lists/', views.ListsRviews.as_view(), name='lists'),
+    path('details/<int:pk>', views.Details.as_view(), name='details'),
+    path('update/<int:pk>', views.Update.as_view(), name='update'),
+    path('delete/<int:pk>', views.Delete.as_view(), name='delete'),
 ]
--- practice_project/testapp/views.py
+++ practice_project/testapp/views.py
@@ -5,7 +5,7 @@ from .models import Review
 
 # Create your views here.
 
-# class based view (VBV)
+# class based view (CBV)
 class Home (generic.TemplateView):
     template_name = 'home.html'
 
@@ -22,4 +22,32 @@ class Form (generic.FormView):
 class CreatePerson (generic.CreateView):
     model = Review
     fields = '__all__'
-    success_url = '/test'
+    success_url = '/test/lists'
+    # template_name= '' # or the default will be : appname/modelname_form.html
+
+class ListsRviews (generic.ListView):
+    model = Review
+    template_name = 'lists.html'
+    context_object_name = 'lists' # defalut = object_list
+    queryset = Review.objects.all().order_by('-id')
+
+    # add context in django CBV
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['var'] = Review.objects.all().order_by('-id')
+        return context
+
+class Details (generic.DetailView):
+    model = Review
+    template_name = 'details.html'
+    context_object_name = 'review'
+
+class Update (generic.UpdateView):
+    model = Review
+    fields = '__all__'
+    success_url = '/test/lists'
+
+class Delete (generic.DeleteView):
+    model = Review
+    success_url = '/test/lists'
+    template_name = 'confirm_delete.html'
