8057af4c542b3fd5d94126edd1322c42ddf5e5a5
BilalQanoa <bilalqanoa@gmail.com>
2026-05-26 10:59:38 +0300

    Putting Everything Together

--- user/users_app/views.py
+++ user/users_app/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
 
 def index (request):
-    return render (request, 'index.html')
+    context = {
+        'users' : User.objects.all()
+    }
+    return render (request, 'index.html', context)
+
+
+def create (request):
+    first_name = request.POST.get('first_name')
+    last_name = request.POST.get('last_name')
+    email = request.POST.get('email')
+    age = request.POST.get('age')
+
+    User.objects.create(first_name=first_name, last_name=last_name, email=email, age=age)
+    return redirect('/')
